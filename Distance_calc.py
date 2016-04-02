131613747834bf806883498837337a3372d5455a
Samir <samir.wdhw@gmail.com>
2016-04-02 15:09:09 +0530

    Initial Commit

---
+++ Distance_calc.py
@@ -0,0 +1,61 @@
+import json, urllib
+import requests
+
+#To be able to pass all locations in one call
+def addLocations(content):
+
+	validArgument = ''
+
+	for locations in content:
+		validArgument += locations + '|'
+
+	return validArgument
+
+
+API_KEY = 'AIzaSyC8m1yNa6eDGE5VLR1GjQ-5Mz_WyoM-5Kc'
+
+#Array of lists to store locations with their distances
+locationDistances = []
+
+#Araay of locations with not found 
+locationNotFound = []
+
+user_origin = "IIT Bombay"
+
+content = [line.rstrip('\n') for line in open('places.txt')]
+
+payload  = {'origins': user_origin, 'destinations': addLocations(content), 'key': API_KEY}
+
+response = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?', params = payload)
+
+
+parsed_json  = response.json()
+
+#A variable to increment with the loop
+i = 0
+
+for row in parsed_json['rows'][0]['elements']:
+	#If a valid path is found
+	if row['status'] == "OK":
+		locationDistances.append(
+									{
+										'location': content[i], 
+										'distance': row['distance']['text']
+									}
+								)
+
+	#If not
+	else:
+		locationNotFound.append(content[i])
+
+	i += 1
+
+
+#To sort the list
+locationDistances.sort()
+
+for i in locationDistances:
+	print i['location']
+
+for notFound in locationNotFound:
+	print notFound
